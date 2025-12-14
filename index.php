e88e41423fa558221c2ae987d5dc414c1ea55508
Maslenko <alexmaslenko@gmail.com>
2025-12-14 20:43:26 +0100

    index updated

--- index.php
+++ index.php
@@ -138,7 +138,28 @@
 	}
  
 
-	echo "<br><h3>#7 Оператор Switch-case</h3>"
+	echo "<br><h3>#7 Оператор Switch-case</h3><br>";
+
+	$x = 0;
+	
+	switch($x) {
+		case 5:	
+			echo "Var: 5";
+			break;
+		case 7:
+			echo "Var: 7";
+			break;
+		case 9:
+			echo "Var: 9";
+			break;
+		case 6:
+			echo "Var: 6";
+			break; 
+		default: 
+			echo "Default Work!";
+			break; 	
+	}
+ 
 
 ?> 
 </body>
