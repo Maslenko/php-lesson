33c02673282128fa151c11c933339a6f0fcb48c2
Maslenko <alexmaslenko@gmail.com>
2026-01-14 22:57:07 +0100

    modified php

--- blocks/header.php
+++ blocks/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Lesson</title>
+	<title><?php echo $title; ?></title> <!-- <?=$title?> сокращенный вариант -->
 	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
