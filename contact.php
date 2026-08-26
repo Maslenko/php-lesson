7dd2fa769672088fd6d2e0bdc88d54f1ac660fc8
Maslenko <alexmaslenko@gmail.com>
2026-08-26 15:25:39 +0200

    Forma obratnoy svyazi

--- contact.php
+++ contact.php
@@ -1,14 +1,24 @@
 <?php
+session_start();
 $title = "Контакты";
 require_once "blocks/header.php"; 
 ?>
 <h1 class="mt-5"><?=$title?></h1>
 
-<form action="check_contact.php" method="post">
-	<input type="text" name="username" value="<?=$_SESSION['username']?>" placeholder="Введите имя" class="form-control"><br>
-	<input type="email" name="email" value="<?=$_SESSION['email']?>" placeholder="Введите электропочту" class="form-control"><br>
-	<input type="text" name="subject" value="<?=$_SESSION['subjeckt']?>" placeholder="Тема сообщения" class="form-control"><br>
-	<textarea name="message" value="<?=$_SESSION['message']?>" placeholder="Ваше сообщение" class="form-control"></textarea><br>
+<form action="check_contact.php" method="post" novalidate="">
+
+	<input type="text" name="username" value="<?=$_SESSION['user_name']?>" placeholder="Введите имя" class="form-control">
+	<div class="text-danger"><?=$_SESSION['error_username']?></div><br>
+
+	<input type="email" name="email" value="<?=$_SESSION['email']?>" placeholder="Введите электропочту" class="form-control">
+	<div class="text-danger"><?=$_SESSION['error_email']?></div><br>
+
+	<input type="text" name="subject" value="<?=$_SESSION['subject']?>" placeholder="Тема сообщения" class="form-control">
+	<div class="text-danger"><?=$_SESSION['error_subject']?></div><br>
+
+	<textarea name="message" placeholder="Ваше сообщение" class="form-control"></textarea>
+	<div class="text-danger"><?=$_SESSION['error_message']?></div><br>
+
 	<button type="submit" class="btn btn-success">Отправить</button>	
 </form>	
 
